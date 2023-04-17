11380c0c15833b9890f390a52ec30de270a51337
parkgunwook <pkw85428@naver.com>
2023-04-17 23:02:24 +0900

    6.알고리즘 학습

---
+++ src/com/algo5/Main2.java
@@ -0,0 +1,17 @@
+package com.algo5;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+
+interface Main2 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+        String S = br.readLine();
+        int i = Integer.parseInt(br.readLine());
+
+        System.out.println(S.charAt(i - 1));
+    }
+}
